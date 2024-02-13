56bb70325f07c0b2f9b66fe49cdc2a6f87f8265f
t-rupe <personal@trupe.info>
2024-02-13 14:55:58 -0800

    Update background color in BooksPage

--- app/page.tsx
+++ app/page.tsx
@@ -189,7 +189,7 @@ export default function BooksPage() {
     }));
   };
   return (
-    <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 bg-gray-100">
+    <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 bg-gray-50">
       <div className="border rounded-lg shadow-sm mb-8 ">
         <div className="p-4 flex justify-between items-center">
           <h2 className="text-lg font-semibold">Books Table</h2>
