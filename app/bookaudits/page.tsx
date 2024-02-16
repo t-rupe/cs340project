67b3c3010951d235cb36cda7dca6e42507777764
t-rupe <personal@trupe.info>
2024-02-16 10:27:24 -0800

    Remove BookAuditPage component

--- app/bookaudits/page.tsx
+++ app/bookaudits/page.tsx
@@ -55,7 +55,7 @@ const BookAuditPage = () => {
     <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
       <div className="border rounded-lg shadow-sm mb-8">
         <div className="p-4 flex justify-between items-center ">
-          <h2 className="text-lg font-semibold">BookAudit</h2>
+          <h2 className="text-lg font-semibold">BookAudits</h2>
           <AddDialog fields={bookAuditFields} />{" "}
         </div>{" "}
         <Table className="bg-white">
